4bafc5175f60da0a6c2b786fa9cd34bca5a97c7d
João Paulo S. Costa <kjuao9@gmail.com>
2020-01-07 14:29:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//https://www.c-sharpcorner.com/technologies
+//https://www.oreilly.com/library/view/head-first-c/9781449358846/ch01.html
+//https://www.oreilly.com/library/view/head-first-c/9781449358846/figs/web/002fig01.png.jpg
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "PROGRAMA";
+            P1.A1();
+            P1.A2();
+            Console.ReadKey();
+        }
+    }
+   public class P1
+    {
+        public static void A1()
+        {
+            Random a = new Random();           
+            
+        //Console.Write(a.Sample());
+        }
+
+        public static void A2()
+        {
+            
+        }
+    }
+}
